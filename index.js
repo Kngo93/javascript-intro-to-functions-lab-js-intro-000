0486ddcb692a369d686713db3a6a62eb6e27920b
Kevin Ngo <k.ngo93@outlook.com>
2018-12-24 04:05:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
-    return "I can\t hear you!";
+  if (string === lowercase) {
+    return "I can\'t hear you!";
   } else if (string.toUpperCase() === uppercase) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
