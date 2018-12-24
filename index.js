cc3e11dd5910d677baa2a9bbfd8feaf5934cfe19
Kevin Ngo <k.ngo93@outlook.com>
2018-12-24 03:59:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  return console.log(string);
+}
+
+function logWhispter(string) {
+  console.log(string.toLowerCase());
+}
+
